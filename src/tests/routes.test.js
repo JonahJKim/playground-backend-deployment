5694f3692bc6ea4495cbd186c5b47100fef1e6bd
Jonah Kim <jonahkim03@gmail.com>
2022-11-24 19:11:44 -0800

    sample failed push

--- src/tests/routes.test.js
+++ src/tests/routes.test.js
@@ -5,7 +5,7 @@ const router = require('../routers/navRouter');
 test('Basic get request', async () => {
     const expected = {
         firstName: "Jonah",
-        lastName: "Kimmmmmmmmmmmmmmmmmmm"
+        lastName: "Jung"
     }
 
     const response = await request(router).get('/');
