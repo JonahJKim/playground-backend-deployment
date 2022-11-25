5f4dc01c7d6cf3de806e7081ee50150e1a8de383
Jonah Kim <jonahkim03@gmail.com>
2022-11-24 17:44:42 -0800

    updated unit testing

--- src/tests/routes.test.js
+++ src/tests/routes.test.js
@@ -6,7 +6,7 @@ test('Basic get request', async () => {
         lastName: "Kim"
     }
 
-    const data = await axios.get('http://localhost:3000')
+    const data = await axios.get('http://172.17.0.1:3000')
         .then((response) => {
             expect(response.data).toEqual(expected);
         })
