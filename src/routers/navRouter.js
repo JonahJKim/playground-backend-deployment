31dab1e479fe784fbcf68345c1b042db2f8e6ed4
Jonah Kim <jonahkim03@gmail.com>
2022-11-30 12:05:03 -0800

    updated a bunch

--- src/routers/navRouter.js
+++ src/routers/navRouter.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get('/', (req, res) => {
     const result = {
-        firstName: "Andrew",
+        firstName: "Jayyyy!",
         lastName: "Kim"
     };
     res.status(200).send(result);
